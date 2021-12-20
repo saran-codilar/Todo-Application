776974b0705ca69bc0a46467a9c4a5b346a2593b
Saran S <94517168+saran-codilar@users.noreply.github.com>
2021-12-20 19:17:33 +0530

    Update Todo.js

--- src/Application/Todo.js
+++ src/Application/Todo.js
@@ -2,131 +2,187 @@ import React, { useEffect, useState } from 'react';
 import Sun from './icon-sun.svg';
 import Moon from './icon-moon.svg';
 import Dark from './bg-desktop-dark.jpg'
+import Light from './bg-desktop-light.jpg'
 import './Header.css';
 
 function Todo() {
 
-
-    const [inputList, setinputList] = useState("");
-    console.log(inputList);
-    const [items, setItems] = useState(getLocalItems());
-    console.log(items);
-
-
-    document.body.style.background = "black";
+    // document.body.style.background = "black";
     const Theme = () => {
 
         var image = document.getElementById('Backgroundchange');
-        if (image.src.match("images/bg-desktop-light")) {
-            image.src = "images/bg-desktop-dark.jpg";
+        if (image.src.match("images/bg-desktop-dark")) {
+            image.src = "images/bg-desktop-light.jpg";
         }
         else {
-            image.src = "images/bg-desktop-light.jpg";
+            image.src = "images/bg-desktop-dark.jpg";
+        }
+
+        var image = document.getElementById('MediaBackgroundchange');
+        if (image.src.match("images/bg-mobile-dark")) {
+            image.src = "images/bg-mobile-light.jpg";
+        }
+        else {
+            image.src = "images/bg-mobile-dark.jpg";
         }
 
         var image = document.getElementById('themechange');
-        if (image.src.match("images/icon-sun")) {
+        if (image.src.match("images/icon-sun.svg")) {
             image.src = "images/icon-moon.svg";
+            document.body.style.background = "white";
         }
         else {
             image.src = "images/icon-sun.svg";
-        }
-
-        if (document.body.style.background === "black") {
-            document.body.style.background = "white";
-        } else {
             document.body.style.background = "black";
         }
 
-        // document.getElementsById('changecolor').style.background="red";
-        // if (document.input.style.background === "black") {
-        //     document.input.style.background = "white";
+        // if (document.body.style.background === "white") {
+        //     document.body.style.background = "black";
         // } else {
-        //     document.input.style.background = "black";',m,m
+        //     document.body.style.background = "white";
         // }
     }
 
-const getLocalItems = () => {
-    let lists = localStorage.getItem('list');
-    if (lists) {
-        return JSON.parse(localStorage.getItem('list'));
-    } else {
-        return [];
-    }
-}
-
-    const itemEvents = (event) => {
-        setinputList(event.target.value);
-    }
-    const addList = () => {
-        setItems((oldItems) => {
-            return [...oldItems, inputList]
-        })
-    }
-
-    useEffect(() => {
-        localStorage.setItem('list', JSON.stringify(items))
-    }, [items])
-
-
-
-    const checkBox = () => {
-        
-        const defaultt = document.getElementById('check');
-        if (defaultt.click) {
-            document.getElementById('scratch').style.textDecoration = "line-through";
-        }else{
-            document.getElementById('scratch').style.textDecoration = "underline";
+    const getLocalItems = () => {
+        const List = localStorage.getItem('List');
+        if (List) {
+            return JSON.parse(localStorage.getItem('List'));
+        }
+        else {
+            return [];
         }
     }
 
-    const DeleteList = (id) => {
-        const updatedItems=items.filter((elem,del)=>{
-            return del !== id;
-            });
-            setItems(updatedItems);  
+    const [InputTodo, setInputTodo] = useState();
+    const [Items, setItems] = useState(getLocalItems());
+    const [TodoFilter, setTodoFilter] = useState('All');
+    const [FilterTodo, setFilterTodo] = useState([]);
+
+    useEffect(() => {
+        localStorage.setItem('List', JSON.stringify(Items));
+        Filter();
+    }, [Items, TodoFilter])
+
+    const Filter = () => {
+        switch (TodoFilter) {
+            case 'Completed':
+                setFilterTodo(Items.filter(el => el.completed === true));
+                break;
+            case 'Active':
+                setFilterTodo(Items.filter(el => el.completed === false));
+                break;
+            default:
+                setFilterTodo(Items);
+                break;
+        }
+    }
+
+    const AddTodo = (Todo) => {
+
+        Todo.preventDefault();
+        if (!InputTodo) {
+            alert('Entered value is Empty')
+        }
+        else {
+            setItems([...Items, { text: InputTodo, completed: false, id: new Date().getTime() }]);
+            setInputTodo();
+        }
+    }
+
+    const Action = (Todo) => {
+        setTodoFilter(Todo.target.textContent)
+    }
+
+    const DeleteTask = (id) => {
+        const UpdatedTaskList = Items.filter((elements, Index) => {
+            return Index != id;
+        })
+        setItems(UpdatedTaskList);
+    }
+
+    const CompletedTask = (id) => {
+
+        const Completed = Items.map((elements) => {
+            if (elements.id === id) {
+
+                return {
+                    ...elements, completed: !elements.completed
+                };
+            }
+            else {
+                return elements;
+            }
+        })
+
+        setItems(Completed);
+    }
+
+    const ClearCompleted = () => {
+        const updatedTodos = Items.filter((val) => {
+            if (val.completed == false) {
+                return val;
+            }
+        });
+        setItems(updatedTodos);
+    };
+
+    const a = () => {
+        a=Items++;
     }
     
 
     return (
-        <div>
+        <div >
             <div className="Background">
-                <img onClick={Theme} src={Dark} id="Backgroundchange" alt="image" />
+                <img src={Light} id="Backgroundchange" alt="image" className="Desktop"/>
+                <img src="images/bg-mobile-light.jpg" className="Media" id="MediaBackgroundchange"/>
             </div>
+                <div className="main-form">
             <div className="Todocontainer">
                 <div className="Header">
                     <h1>TODO</h1>
                     <img onClick={Theme} src={Moon} id="themechange" alt="image" className="Sun" />
                 </div>
-                <form>
-                    <label className="Inputbox">
-                        <input type="text" placeholder="Create a new to do" className="Input" id="colorchange" onChange={itemEvents}/>
-                        <input onClick={addList} className="Submit" type="submit" />
-                    </label>
-                </form>
             </div>
 
-            <div className="entry">
+            <form onSubmit={AddTodo}>
+                <lable className="Inputbox"  >
+                    <input type="text" placeholder="Create a new todo..." className="Input" value={InputTodo}
+                        onChange={(Todo) => {
+                            setInputTodo(Todo.target.value)
+                        }}
+                    ></input>
+                </lable>
+            </form>
+
+            <div className="Todolist">
                 <ol className='list'>
 
-                    {items.map((itemVal,del) => {
-                        return <li key={del}><input type="checkbox" value="" onClick={checkBox} id="check"/>{itemVal}
-                        <span><img src="images/icon-cross.svg" alt="Close" onClick={()=>DeleteList(del)} /></span>
-                        </li>
-                    })}
-                                      
-                     
+                {
+                        FilterTodo.map((elements, i) => {
+                            return (
+                                <li key={i} className="List">
+                                    <input type="checkbox" className="CheckItem" checked={elements.completed} onClick={() => CompletedTask(elements.id)} />
+                                    <span className="Inputholder">{elements.text}</span>
+                                    <span><img src="images/icon-cross.svg" alt="Close" onClick={() => DeleteTask(i)}/></span>
+                                </li>
+                            )
+
+                        })
+                    }
+            
+                    <div className="Function">
+                        <span className="Hover" >Items Left</span>
+                        <span className="Hover" onClick={Action}>All</span>
+                        <span className="Hover" onClick={Action}>Active</span>
+                        <span className="Hover" onClick={Action}>Completed</span>
+                        <span className="Hover" onClick={ClearCompleted}>Clear Completed</span>
+                    </div>
+
                 </ol>
 
             </div>
-            <div className="function">
-                <span className="hover">Items Left</span>
-                <span className="hover">All</span>
-                <span className="hover">Active</span>
-                <span className="hover">Completed</span>
-                <span className="hover">Clear Completed</span>
             </div>
-
         </div>
     )
 }
